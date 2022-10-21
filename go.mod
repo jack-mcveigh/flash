a135e58d4e1f1e6994bf432d351a80048a709cac
Jack McVeigh <jmcveigh55@gmail.com>
2022-10-21 19:37:38 -0400

    Added json get cards

--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module github.com/jmcveigh55/flash
 
 go 1.19
 
-require github.com/urfave/cli/v2 v2.20.2
+require (
+	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975
+	github.com/urfave/cli/v2 v2.20.2
+)
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
--- pkg/storage/json/repository.go
+++ pkg/storage/json/repository.go
@@ -1,4 +1,55 @@
 package json
 
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/jmcveigh55/flash/pkg/core/adding"
+	"github.com/jmcveigh55/flash/pkg/core/deleting"
+	"github.com/jmcveigh55/flash/pkg/core/getting"
+	scribble "github.com/nanobox-io/golang-scribble"
+)
+
+const (
+	dataPath       = "/tmp/flash"
+	CardCollection = "card"
+)
+
+var ErrCardNotFound error = errors.New("Card not found")
+
 type Repository struct {
+	db *scribble.Driver
+}
+
+func New() (*Repository, error) {
+	db, err := scribble.New(dataPath, nil)
+	return &Repository{db}, err
+}
+
+func (r *Repository) AddCard(c adding.Card) error {
+	return nil
+}
+
+func (r *Repository) DeleteCard(c deleting.Card) error {
+	return nil
+}
+
+func (r *Repository) GetCards() []getting.Card {
+	cards := []getting.Card{}
+	records, err := r.db.ReadAll(CardCollection)
+	if err != nil {
+		// TODO: Add error handling
+		return cards
+	}
+
+	for _, r := range records {
+		var c Card
+		if err := json.Unmarshal([]byte(r), &c); err != nil {
+			// TODO: Add error handling
+			return cards
+		}
+
+		cards = append(cards, getting.Card{Title: c.Title, Desc: c.Desc})
+	}
+	return cards
 }
