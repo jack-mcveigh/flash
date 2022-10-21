2a6dd6bbd7324517db0f4e4f4937dd38df8ed0b3
Jack McVeigh <jmcveigh55@gmail.com>
2022-10-21 00:10:04 -0400

    Added in memory repo, tests

--- pkg/storage/memory/repository.go
+++ pkg/storage/memory/repository.go
@@ -1,4 +1,54 @@
 package memory
 
+import (
+	"errors"
+
+	"github.com/jmcveigh55/flash/pkg/core/adding"
+	"github.com/jmcveigh55/flash/pkg/core/deleting"
+	"github.com/jmcveigh55/flash/pkg/core/getting"
+)
+
+var ErrCardNotFound error = errors.New("Card not found")
+
 type Repository struct {
+	cards []Card
+}
+
+func NewRepository() *Repository {
+	return &Repository{}
+}
+
+func (r *Repository) AddCard(c adding.Card) error {
+	r.cards = append(
+		r.cards,
+		Card{Title: c.Title, Desc: c.Desc},
+	)
+	return nil
+}
+
+func (r *Repository) DeleteCard(c deleting.Card) error {
+	index := -1
+	for i, card := range r.cards {
+		if c.Title == card.Title {
+			index = i
+		}
+	}
+
+	if index < 0 {
+		return ErrCardNotFound
+	}
+
+	r.cards = append(r.cards[:index], r.cards[index+1:]...)
+	return nil
+}
+
+func (r *Repository) GetCards() []getting.Card {
+	var cards []getting.Card
+	for _, c := range r.cards {
+		cards = append(cards, getting.Card{
+			Title: c.Title,
+			Desc:  c.Desc,
+		})
+	}
+	return cards
 }
--- pkg/storage/memory/repository_test.go
+++ pkg/storage/memory/repository_test.go
@@ -1,1 +1,195 @@
 package memory
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jmcveigh55/flash/pkg/core/adding"
+	"github.com/jmcveigh55/flash/pkg/core/deleting"
+	"github.com/jmcveigh55/flash/pkg/core/getting"
+)
+
+func newRepositoryWithCards() *Repository {
+	r := NewRepository()
+	r.cards = append(
+		r.cards,
+		Card{Title: "Subject1", Desc: "Value1"},
+		Card{Title: "Subject2", Desc: "Value2"},
+		Card{Title: "Subject3", Desc: "Value3"},
+	)
+	return r
+}
+
+func TestAddCardSingle(t *testing.T) {
+	tests := []struct {
+		name    string
+		card    adding.Card
+		want    []Card
+		wantErr error
+	}{
+		{
+			name: "Normal",
+			card: adding.Card{Title: "Subject4", Desc: "Value4"},
+			want: []Card{
+				{Title: "Subject1", Desc: "Value1"},
+				{Title: "Subject2", Desc: "Value2"},
+				{Title: "Subject3", Desc: "Value3"},
+				{Title: "Subject4", Desc: "Value4"},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Empty Title",
+			card: adding.Card{Title: "", Desc: "Value4"},
+			want: []Card{
+				{Title: "Subject1", Desc: "Value1"},
+				{Title: "Subject2", Desc: "Value2"},
+				{Title: "Subject3", Desc: "Value3"},
+				{Title: "", Desc: "Value4"},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Empty Desc",
+			card: adding.Card{Title: "Subject4", Desc: ""},
+			want: []Card{
+				{Title: "Subject1", Desc: "Value1"},
+				{Title: "Subject2", Desc: "Value2"},
+				{Title: "Subject3", Desc: "Value3"},
+				{Title: "Subject4", Desc: ""},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := newRepositoryWithCards()
+			if err := r.AddCard(tt.card); err != nil {
+				if tt.wantErr != err {
+					t.Errorf("Incorrect error. Want %v, got %v", tt.wantErr, err)
+				}
+			}
+
+			if !reflect.DeepEqual(tt.want, r.cards) {
+				t.Errorf("Incorrect cards. Want %v, got %v", tt.want, r.cards)
+			}
+		})
+	}
+}
+
+func TestAddCardMultiple(t *testing.T) {
+	tests := []struct {
+		name    string
+		cards   []adding.Card
+		want    []Card
+		wantErr error
+	}{
+		{
+			name: "Normal",
+			cards: []adding.Card{
+				{Title: "Subject4", Desc: "Value4"},
+				{Title: "Subject5", Desc: "Value5"},
+			},
+			want: []Card{
+				{Title: "Subject1", Desc: "Value1"},
+				{Title: "Subject2", Desc: "Value2"},
+				{Title: "Subject3", Desc: "Value3"},
+				{Title: "Subject4", Desc: "Value4"},
+				{Title: "Subject5", Desc: "Value5"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := newRepositoryWithCards()
+			for _, c := range tt.cards {
+				r.AddCard(c)
+			}
+
+			if !reflect.DeepEqual(tt.want, r.cards) {
+				t.Errorf("Incorrect cards. Want %v, got %v", tt.want, r.cards)
+			}
+		})
+	}
+}
+
+func TestDeleteCard(t *testing.T) {
+	tests := []struct {
+		name    string
+		card    deleting.Card
+		want    []Card
+		wantErr error
+	}{
+		{
+			name: "Delete First",
+			card: deleting.Card{Title: "Subject1"},
+			want: []Card{
+				{Title: "Subject2", Desc: "Value2"},
+				{Title: "Subject3", Desc: "Value3"},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Delete Last",
+			card: deleting.Card{Title: "Subject3"},
+			want: []Card{
+				{Title: "Subject1", Desc: "Value1"},
+				{Title: "Subject2", Desc: "Value2"},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := newRepositoryWithCards()
+			if err := r.DeleteCard(tt.card); err != nil {
+				if tt.wantErr != err {
+					t.Errorf("Incorrect error. Want %v, got %v", tt.wantErr, err)
+				}
+			}
+
+			if !reflect.DeepEqual(tt.want, r.cards) {
+				t.Errorf("Incorrect cards. Want %v, got %v", tt.want, r.cards)
+			}
+		})
+	}
+}
+
+func TestGetCards(t *testing.T) {
+	tests := []struct {
+		name string
+		want []getting.Card
+	}{
+		{
+			name: "Single Card",
+			want: []getting.Card{
+				{Title: "Subject1", Desc: "Value1"},
+			},
+		},
+		{
+			name: "Multiple Cards",
+			want: []getting.Card{
+				{Title: "Subject1", Desc: "Value1"},
+				{Title: "Subject2", Desc: "Value2"},
+				{Title: "Subject3", Desc: "Value3"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := NewRepository()
+			for _, c := range tt.want {
+				r.cards = append(r.cards, Card{Title: c.Title, Desc: c.Desc})
+			}
+			cards := r.GetCards()
+
+			if !reflect.DeepEqual(tt.want, cards) {
+				t.Errorf("Incorrect cards. Want %v, got %v", tt.want, r.cards)
+			}
+		})
+	}
+}
