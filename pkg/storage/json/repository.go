aa92d8a296229ec86c5870ca20393e55b01be219
Jack McVeigh <jmcveigh55@gmail.com>
2022-10-22 19:36:19 -0400

    Changed repo to use interface for db driver

--- pkg/storage/json/repository.go
+++ pkg/storage/json/repository.go
@@ -3,8 +3,6 @@ package json
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
-	"os"
 
 	"github.com/jmcveigh55/flash/pkg/core/adding"
 	"github.com/jmcveigh55/flash/pkg/core/deleting"
@@ -13,14 +11,20 @@ import (
 )
 
 const (
+	cardCollection = "card"
 	dataPath       = "/tmp/flash"
-	CardCollection = "card"
 )
 
 var ErrCardNotFound error = errors.New("Card not found")
 
+type dbDriver interface {
+	Write(string, string, any) error
+	ReadAll(string) ([]string, error)
+	Delete(string, string) error
+}
+
 type Repository struct {
-	db *scribble.Driver
+	db dbDriver
 }
 
 func New() (*Repository, error) {
@@ -34,7 +38,7 @@ func (r *Repository) AddCard(c adding.Card) error {
 		Desc:  c.Desc,
 	}
 
-	if err := r.db.Write(CardCollection, card.Title, card); err != nil {
+	if err := r.db.Write(cardCollection, card.Title, card); err != nil {
 		// TODO: Add error handling
 		return err
 	}
@@ -42,13 +46,13 @@ func (r *Repository) AddCard(c adding.Card) error {
 }
 
 func (r *Repository) DeleteCard(c deleting.Card) error {
-	p := fmt.Sprintf("%s/%s/%s.json", dataPath, CardCollection, c.Title)
-	return os.Remove(p)
+	// TODO: Add error handling
+	return r.db.Delete(cardCollection, c.Title)
 }
 
 func (r *Repository) GetCards() []getting.Card {
 	cards := []getting.Card{}
-	records, err := r.db.ReadAll(CardCollection)
+	records, err := r.db.ReadAll(cardCollection)
 	if err != nil {
 		// TODO: Add error handling
 		return cards
