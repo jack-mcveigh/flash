1b58c6cbc23ac97f76bf9c0f7941f0cdf6b03c23
Jack McVeigh <jmcveigh55@gmail.com>
2022-10-22 22:35:16 -0400

    Removing unnecessary test

--- pkg/core/adding/service_test.go
+++ pkg/core/adding/service_test.go
@@ -14,7 +14,7 @@ func (r *repositoryStub) AddCard(c Card) error {
 	return nil
 }
 
-func TestAddCardSingle(t *testing.T) {
+func TestAddCard(t *testing.T) {
 	tests := []struct {
 		name    string
 		card    Card
@@ -48,51 +48,6 @@ func TestAddCardSingle(t *testing.T) {
 			err := as.AddCard(tt.card)
 			if err != tt.wantErr {
 				t.Errorf("Incorrect error. Want %v, got %v", tt.wantErr, err)
-			}
-
-			if !reflect.DeepEqual(tt.want, repo.cards) {
-				t.Errorf("Incorrect repo.cards. Want %v, got %v", tt.want, repo.cards)
-			}
-		})
-	}
-}
-
-func TestAddCardMultiple(t *testing.T) {
-	tests := []struct {
-		name  string
-		cards []Card
-		want  []Card
-	}{
-		{
-			name: "Normal",
-			cards: []Card{
-				{Title: "Subject1", Desc: "Value1"},
-				{Title: "Subject2", Desc: "Value2"},
-			},
-			want: []Card{
-				{Title: "Subject1", Desc: "Value1"},
-				{Title: "Subject2", Desc: "Value2"},
-			},
-		},
-		{
-			name: "One Empty Title",
-			cards: []Card{
-				{Title: "", Desc: "Value1"},
-				{Title: "Subject2", Desc: "Value2"},
-			},
-			want: []Card{
-				{Title: "Subject2", Desc: "Value2"},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			repo := &repositoryStub{}
-			as := New(repo)
-
-			for _, card := range tt.cards {
-				as.AddCard(card)
 			}
 
 			if !reflect.DeepEqual(tt.want, repo.cards) {
